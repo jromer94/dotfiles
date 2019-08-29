e03f5093b35cb4cd44b6c53d3cc8b99e9fe156d8
Josh Romer <jromer94@gmail.com>
2019-08-29 15:06:09 -0400

    added script to compile alacritty

---
+++ init/alacritty.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3echo "Cloning alacritty"
+git clone https://github.com/jwilm/alacritty.git ~/alacritty > /dev/null
+
+cd ~/alacritty
+git pull > /dev/null
+
+echo "Building alacritty"
+cargo install cargo-deb
+echo "Installing alacritty deb file"
+cargo deb --install --manifest-path alacritty/Cargo.toml
