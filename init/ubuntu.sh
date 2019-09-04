994a9ac89b1442968ab3df8ac00397209c66e72a
Josh Romer <jromer94@gmail.com>
2019-09-04 17:32:36 -0400

    added curl and git installs

--- init/ubuntu.sh
+++ init/ubuntu.sh
@@ -6,3 +6,6 @@ sudo sed -i~orig -e 's/# deb-src/deb-src/' /etc/apt/sources.list
 
 echo "Updating packages"
 sudo apt-get -y update > /dev/null
+
+echo "Installing basic dependencies i.e git, curl"
+sudo apt-get -y install git curl > /dev/null
